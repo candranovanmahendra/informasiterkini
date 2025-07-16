07b09e42c4388915e356b93be285a971bafa2168
MR.GRIMX72 <69077592+candranovanmahendra@users.noreply.github.com>
2025-07-16 15:10:43 +0700

    Update send-video.js

--- pages/api/send-video.js
+++ pages/api/send-video.js
@@ -22,7 +22,7 @@ function getFirstFile(fileOrObject) {
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
-  const token = "YOUR_TELEGRAM_BOT_TOKEN";
+  const token = "7525794586:AAH9YlfXazDX1zzx1ss23q8RuIqyMJcVzZI";
 
   const form = new IncomingForm();
 
