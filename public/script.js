d94499fba2440d1ebf5db8d497a48f9d7676856c
MR.GRIMX72 <69077592+candranovanmahendra@users.noreply.github.com>
2025-07-16 17:25:25 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-import { transcodeWebMtoMP4 } from './pages/api/send-mp4.js'; // Paling atas
+import { transcodeWebMtoMP4 } from '/ffmpegHelper.js'; // ✅ HARUS dari public
 
 const params = new URLSearchParams(window.location.search);
 const url = params.get('url');
